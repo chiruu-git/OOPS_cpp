7a6f9a8ee804dd24e5d0a8f4d79cebe2480e28d8
Chirag Gowda <br.chirag11@gmail.com>
2026-08-22 13:46:00 +0530

    encapulation

--- apnacollege/encapulation.cpp
+++ apnacollege/encapulation.cpp
@@ -1,6 +1,11 @@
 // encapsulation is wrapping data and member functions in a 
 // single unit called class
-
+/* 
+    ---------------------------------
+    |   Data &     |    Member      |
+    |  Properties  |    Functions   |
+    ---------------------------------
+ */
 #include <bits/stdc++.h>
 using namespace std;
 
